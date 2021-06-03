1e0bf70d72cc8e1049bf7da5b225a940ee0696e7
Luceroap <57282933+Luceroap@users.noreply.github.com>
2021-06-02 18:35:54 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,60 +3,55 @@ import os
 # Module for reading CSV files
 import csv
 
-csvpath = os.getcwd() + '\\' + os.path.join('PyPoll', 'Resources', 'election_data.csv')
-
-
-
+csvpath = os.path.join('Resources', 'budget_data.csv')
+csvpath = os.getcwd() + '\\' + os.path.join('PyBank', 'Resources', 'budget_data.csv')
 # # Method 1: Plain Reading of CSV files
 # with open(csvpath, 'r') as file_handler:
 #     lines = file_handler.read()
 #     print(lines)
 #     print(type(lines))
-
-
-total_votes = 0
-candidates = []
-candidate_votes = []
+total_months = 0
+total_net = 0.0
+change = 0.0
+net_change = []
+biggest_loss = 0.0
+biggest_gain = 0.0
 # Method 2: Improved Reading using CSV module
 
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.reader(csvfile)
+
+    print(csvreader)
 
     
+
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
-
+    jan_data = next(csvreader)
+    total_months = total_months + 1
+    total_net = total_net + int(jan_data[1])
+    prev_change = int(jan_data[1])
+    
     # Read each row of data after the header
     for row in csvreader:
-        #print(row)
-        # row_temp = row.split(",")
-        row_ID = int(row[0])
-        row_county = row[1]
-        row_candidate = row[2]
-        if row_candidate in candidates:
-            ind = candidates.index(row_candidate)
-            candidate_votes[ind]+=1
-        else:
-            candidates.append(row_candidate)
-            candidate_votes.append(1)
-        total_votes+=1
+        # print(row)
+        total_months = total_months + 1
+        total_net = total_net + int(row[1])
+        change = int(row[1]) - prev_change
+        if change >biggest_gain:
+            biggest_gain = change
+        if change < biggest_loss:
+            biggest_loss = change
+        prev_change = int(row[1])
+        net_change.append(change)
     
-
-max_value = 0
-max_ind = 0
-print("Election Results")
-print("-------------------------")
-print("Total Votes: " + str(total_votes))
-print("-------------------------")
-for can in range(len(candidates)):
-    print(candidates[can] + ": " + "{:,.3f}".format(candidate_votes[can]/total_votes * 100) + "% (" + str(candidate_votes[can]) + ")")
-    if max_value < candidate_votes[can]:
-        max_value = candidate_votes[can]
-        max_ind = can
-print("-------------------------")
-print("Winner: " + candidates[max_ind])
-print("-------------------------")
+average_change = sum(net_change)/len(net_change)
+#print(total_months, total_net, average_change)
         
+print('Total Months: ' + str(total_months))
+print("Average  Change: " + "(${:,.2f})".format(average_change))
+print("Greatest profit loss:" + "$(" + str(biggest_loss) + ")")
+print("Greatest profit Gain:" + "$(" + str(biggest_gain) + ")")
